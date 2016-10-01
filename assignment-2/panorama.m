edc95d82733fdbc2c495aba7ac242f168b543b17
Sunil <sunhick@gmail.com>
2016-09-30 21:36:59 -0600

    modified strings

--- assignment-2/panorama.m
+++ assignment-2/panorama.m
@@ -15,9 +15,10 @@ if choice == 1
     return;
 end
 
-% read first image
+% select image-1 that needs to be warped into image-2 plane of
+% reference
 [imageFile1, pathname] = ...
-    uigetfile({'*.jpg';'*.png';'*.bmp'},'Choose image 1');
+    uigetfile({'*.jpg';'*.png';'*.bmp'},'Choose image 1 to be warped');
 if isequal(imageFile1,0)
   return;
 else
@@ -30,9 +31,9 @@ if choice ~= 3
     pts1 = ginput2(4);
 end
 
-% read second image
+% select image-2 that serves as reference image.
 [imageFile2, pathname] = ...
-    uigetfile({'*.jpg';'*.png';'*.bmp'},'Choose image 2');
+    uigetfile({'*.jpg';'*.png';'*.bmp'},'Choose image 2(reference)');
 if isequal(imageFile2,0)
   return;
 else
@@ -48,14 +49,17 @@ switch choice
     case 2 % Image Mosaic
         H = computeHomography(pts1, pts2);
         output = imageWarp(image1, image2, H, 'MOSAIC');
+        imwrite(output, 'mosaic.jpg');
         
     case 3 % RANSAC
         H = ransac(image1, image2);
         output = imageWarp(image1, image2, H, 'MOSAIC');
+        imwrite(output, 'ransac.jpg');
         
     case 4 % Frame image to billboard
         H = computeHomography(pts1, pts2);
         output = imageWarp(image1, image2, H, 'FRAME');
+        imwrite(output, 'frame.jpg');
 end
 
 figure;
